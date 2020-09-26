83cefc0136b98733d36e9d8b28a4d246a2a9fe5a
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2020-09-26 19:38:49 +0000

    last state
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: watchtheblur <4589786+watchtheblur@users.noreply.github.com>
    Co-Authored-By: Llewellyn Falco <10874+isidore@users.noreply.github.com>
    Co-Authored-By: rzijp <7345541+rzijp@users.noreply.github.com>
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Valentin Iliescu <1235359+valentiniliescu@users.noreply.github.com>

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -1,7 +1,20 @@
+using Refactoring.Pipelines;
+using Refactoring.Pipelines.ApprovalTests;
+
 static class FizzBuzz
 {
     public static string Convert(int i)
     {
+        // Set up Pipeline
+        var inputpipe = new InputPipe<int>("number");
+        var pipe = inputpipe.Process(_ => _.ToString());
+        var methodCallCollector = pipe.Collect();
+        inputpipe.Send(i);
+        var variable = methodCallCollector.SingleResult;
+        // ApprovalPipeline
+        PipelineApprovals.Verify(inputpipe);
+        // Send thru pipeline
+        // Original code
         return i.ToString();
     }
 }
--- FizzBuzzKata/UnitTest1.cs
+++ FizzBuzzKata/UnitTest1.cs
@@ -1,18 +1,85 @@
+using System;
+using System.Linq;
+using ApprovalTests;
+using ApprovalTests.Reporters;
 using ApprovalUtilities.Persistence;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NuGet.Frameworks;
+using Refactoring.Pipelines;
+using Refactoring.Pipelines.ApprovalTests;
+using Refactoring.Pipelines.InputsAndOutputs;
 
 namespace FizzBuzzKata
 {
     [TestClass]
+    [UseReporter(typeof(BeyondCompareReporter))]
     public class UnitTest1
     {
-        [TestMethod]
-        public void FizzBuzzTest()
-        {
-            var result = FizzBuzz.Convert(1);
-            Assert.AreEqual(result, "1");
+        //    [TestMethod]
+        //    public void FizzBuzzTest()
+        //    {
+        //        var result = FizzBuzz.Convert(1);
+        //        Assert.AreEqual(result, "1");
 
+        //    }
+
+        //------------------------------- Step 1, create this template
+        [TestMethod]
+        public void TestPipeline()
+        {
+            PipelineApprovals.Verify(CreatePipeline());
+
+        }
+
+        IGraphNode CreatePipeline()
+        {
+            return new InputPipe<object>("input");
+        }
+
+        [TestMethod]
+        public void FizzBuzzPipeline()
+        {
+            var fizzBuzzPipeline = CreateFizzBuzzPipeline();
+            PipelineApprovals.Verify(fizzBuzzPipeline);
+        }
+
+        [TestMethod]
+        public void TestSystem()
+        {
+            Approvals.VerifyAll(Enumerable.Range(1, 20), i => $"{i} => {Convert(i)}");
+        }
+
+        string Convert(int i)
+        {
+            var fizzBuzzPipeline = CreateFizzBuzzPipeline();
+            var inputsAndOutputs = fizzBuzzPipeline.GetInputs<int>().AndOutputs<string>();
+            inputsAndOutputs.Send(i);
+            return inputsAndOutputs.Output1.SingleResult;
+
+        }
+        private IGraphNode CreateFizzBuzzPipeline()
+        {
+            var inputPipe = new InputPipe<int>("number");
+            var fizzPipe = inputPipe.ProcessFunction(FizzConverter);
+            var buzzPipe = inputPipe.ProcessFunction(BuzzConverter);
+            var joinedPipe = fizzPipe.JoinTo(buzzPipe).Process((a, b) => a + b);
+            var joinedPipe2 = joinedPipe.JoinTo(inputPipe).ProcessFunction(DefaultPipe);
+            joinedPipe2.Collect();
+            return inputPipe;
+        }
+
+        private string FizzConverter(int arg)
+        {
+            return arg % 3 == 0 ? "Fizz" : "";
+        }
+        private string BuzzConverter(int arg)
+        {
+            return arg % 5 == 0 ? "Buzz" : "";
+        }
+
+        string DefaultPipe(Tuple<string, int> stringAndNumber)
+        {
+            return stringAndNumber.Item1 == "" ? stringAndNumber.Item2.ToString() : stringAndNumber.Item1;
         }
     }
 }
